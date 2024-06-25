5b1ecc1fcb01b441bcd9efd8a91f60f90882ef8f
camziny <cameronziny@gmail.com>
2024-06-25 10:49:29 -0400

    Responsiveness
    - make pagination more responsive
    - edit readme

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -30,43 +30,81 @@ const PaginationControls: React.FC<PaginationControlsProps> = ({
     onPageChange(page);
   };
 
+  const getVisiblePages = (
+    totalPages: number,
+    currentPage: number,
+    visiblePageCount: number
+  ) => {
+    const startPage = Math.max(
+      1,
+      currentPage - Math.floor(visiblePageCount / 2)
+    );
+    const endPage = Math.min(totalPages, startPage + visiblePageCount - 1);
+    const pages = [];
+    for (let i = startPage; i <= endPage; i++) {
+      pages.push(i);
+    }
+    return pages;
+  };
+
+  const smallScreenPages = getVisiblePages(totalPages, currentPage, 5);
+  const largeScreenPages = getVisiblePages(totalPages, currentPage, 10);
+
   return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              if (hasPrevPage) handlePageChange(currentPage - 1);
-            }}
-          />
-        </PaginationItem>
-        {Array.from({ length: totalPages }, (_, i) => (
-          <PaginationItem key={i + 1}>
-            <PaginationLink
+    <div className="mt-8 flex justify-center px-4">
+      <Pagination className="flex flex-wrap justify-center items-center space-x-2">
+        <PaginationContent className="flex items-center">
+          <PaginationItem className="m-1">
+            <PaginationPrevious
               href="#"
+              className="p-2"
               onClick={(e) => {
                 e.preventDefault();
-                handlePageChange(i + 1);
+                if (hasPrevPage) handlePageChange(currentPage - 1);
               }}
-              isActive={i + 1 === currentPage}
-            >
-              {i + 1}
-            </PaginationLink>
+            />
           </PaginationItem>
-        ))}
-        <PaginationItem>
-          <PaginationNext
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              if (hasNextPage) handlePageChange(currentPage + 1);
-            }}
-          />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
+          {smallScreenPages.map((page) => (
+            <PaginationItem key={page} className="m-1 sm:hidden">
+              <PaginationLink
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handlePageChange(page);
+                }}
+                isActive={page === currentPage}
+              >
+                {page}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          {largeScreenPages.map((page) => (
+            <PaginationItem key={page} className="m-1 hidden sm:inline-block">
+              <PaginationLink
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handlePageChange(page);
+                }}
+                isActive={page === currentPage}
+              >
+                {page}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+          <PaginationItem className="m-1">
+            <PaginationNext
+              href="#"
+              className="p-2"
+              onClick={(e) => {
+                e.preventDefault();
+                if (hasNextPage) handlePageChange(currentPage + 1);
+              }}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
   );
 };
 
